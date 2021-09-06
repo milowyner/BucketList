95dcb22907f9d6950da6608aa6d2a4c790ada446
Milo Wyner <milowyner@gmail.com>
2021-09-06 14:07:54 -0700

    Create MapView to show an MKMapView, then display it in ContentView with a transparent circle in the center and a button in the bottom right which adds a pin on the map when pressed

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,11 +6,41 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        newLocation.coordinate = centerCoordinate
+                        locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+        }
     }
 }
 
