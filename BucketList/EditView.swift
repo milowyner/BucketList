3e7c62359f3aa40222c1508d40479dd8bafb192c
Milo Wyner <milowyner@gmail.com>
2021-09-07 11:47:52 -0700

    Update EditView so when it appears it fetches nearby locations using a Wikipedia API

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 import MapKit
 
 struct EditView: View {
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var placemark: MKPointAnnotation
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
 
     var body: some View {
         NavigationView {
@@ -19,12 +25,56 @@ struct EditView: View {
                     TextField("Place name", text: $placemark.wrappedTitle)
                     TextField("Description", text: $placemark.wrappedSubtitle)
                 }
+                
+                Section(header: Text("Nearby...")) {
+                    if loadingState == .loaded {
+                        List(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text("Page description here")
+                                .italic()
+                        }
+                    } else if loadingState == .loading {
+                        Text("Loading...")
+                    } else {
+                        Text("Please try again later.")
+                    }
+                }
             }
             .navigationBarTitle("Edit place")
             .navigationBarItems(trailing: Button("Done") {
                 self.presentationMode.wrappedValue.dismiss()
             })
+            .onAppear(perform: fetchNearbyPlaces)
         }
+    }
+    
+    func fetchNearbyPlaces() {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(placemark.coordinate.latitude)%7C\(placemark.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            loadingState = .failed
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let items = try JSONDecoder().decode(Result.self, from: data)
+                    pages = Array(items.query.pages.values)
+                    loadingState = .loaded
+                    return
+                } catch {
+                    print(error.localizedDescription)
+                }
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            loadingState = .failed
+        }.resume()
     }
 }
 
